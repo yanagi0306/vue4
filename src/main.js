e0e45a4537176c0195b266e6afaa6fa9af819826
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-05-28 09:45:22 +0900

    vue4-12

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
 
 Vue.config.productionTip = false
+const firebaseConfig = {
+  apiKey: "AIzaSyARuAyDjoJpUmMIPQhYSga8qEE_6QhWyQM",
+  authDomain: "vue4-4ff42.firebaseapp.com",
+  projectId: "vue4-4ff42",
+  storageBucket: "vue4-4ff42.appspot.com",
+  messagingSenderId: "672732277341",
+  appId: "1:672732277341:web:658d69196d73a1af97bbb4",
+  measurementId: "G-C3GHL7ER7X"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
